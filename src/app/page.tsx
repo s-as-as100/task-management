0e260c21ca3bf96dd06dee618d18ecdb0afedbb3
Mohd Arif <s.arifsaifi000@gmail.com>
2025-02-21 21:07:23 +0530

    add loader and suspense to solve deploy error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Dashboard from "@/Components/Dashboard/Dashboard";
 import styles from "./page.module.scss";
-
+import { Suspense } from "react";
+import Image from "next/image";
+import loader from "./../assets/images/Spinner-2.gif";
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <Dashboard />
-    </div>
+    <Suspense fallback={<Image src={loader} alt="...loader" />}>
+      <div className={styles.page}>
+        <Dashboard />
+      </div>
+    </Suspense>
   );
 }
